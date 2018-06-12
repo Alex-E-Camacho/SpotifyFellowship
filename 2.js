dacf7c96aec6a584aec457a4449937888d37c6df
Alex-E-Camacho <alex.e.camacho@gmail.com>
2018-06-12 13:02:36 -0700

    Format for readability

--- 2.js
+++ 2.js
@@ -1,20 +1,20 @@
 const decodeString = (s) => {
   let sArray = s.split('');
   let times = sArray.length - 1;
-  let output = '';
+  let outputString = '';
 
-  for ( i = (times); i > -1; i--) {
-    if (sArray[i].match(/[a-z]/i)) {
-        output = sArray[i] + output;
+  for ( i = times; i > -1; i-- ) {
+    if ( sArray[i].match(/[a-z]/i) ) {
+      outputString = sArray[i] + outputString;
     } else if (!isNaN(sArray[i])) {
-      output = output.repeat(parseInt(sArray[i]));
+      outputString = outputString.repeat(parseInt(sArray[i]));
     }
   }
-  return output;
+  return outputString;
 }
 
-let test = "2[b3[a]]";
+let test1 = "2[b3[a]]";
 let test2 = "4[ab]";
 
-console.log(decodeString(test));
+console.log(decodeString(test1));
 console.log(decodeString(test2));
